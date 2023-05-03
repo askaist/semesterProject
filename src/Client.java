c415adb046acdd12ea36b4a9a9881040d96cc023
AriBinyamin <aribinyamin@gmail.com>
2023-05-03 00:04:03 -0400

    I connected the Server and Client together. Multiple clients will be able to connect because we have a ServerThreads class that will ensure that the server can be connected to multiple clients Slaves class has been created so that we can create to slaves that are optimized for different jobs. User/ client can input job type.

--- src/Client.java
+++ src/Client.java
@@ -1,68 +1,55 @@
 import threads.ClientRequestThread;
-import threads.ClientResponseThread;
 
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Objects;
-
 public class Client {
 
     public static void main(String[] args) {
 
-        String[] connectionArray = new String[]{"127.0.0.1", "30121"};
+        args = new String[] { "127.0.0.1", "30121" };
 
-        if (connectionArray.length != 2) {
-            System.err.println(
-                    "Usage: java EchoClient <host name> <port number>");
+        if (args.length != 2) {
+            System.err.println("Usage: java EchoClient <host name> <port number>");
             System.exit(1);
         }
 
-        String hostName = connectionArray[0];
-        int portNumber = Integer.parseInt(connectionArray[1]);
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
 
         String jobType = args[0];
 
-        if (!Objects.equals(jobType, "a") && !Objects.equals(jobType, "b")) {
-            System.err.println(
-                    "Invalid job type submitted");
-            System.exit(1);
-        }
+        try (Socket clientSocket = new Socket(hostName, portNumber);
+             PrintWriter requestWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader responseReader = new BufferedReader(
+                     new InputStreamReader(clientSocket.getInputStream()));) {
 
-        try (
-                Socket clientSocket = new Socket(hostName, portNumber);
-                PrintWriter requestWriter = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader responseReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                ) {
+            //Get input from client
+            System.out.println("Enter a job value: A or B ");
+            String message = responseReader.readLine().strip();
+            // sends message to server
+            requestWriter.println(message);
 
+            if (!Objects.equals(jobType, "A") && !Objects.equals(jobType, "B")) {
+                System.err.println("Invalid job type submitted");
+                System.exit(1);
+            }
+//            Thread requestWriterThread = new ClientRequestThread(requestWriter, jobType);
+//            Thread responseReaderThread = new ClientResponseThread(responseReader);
 
-            Thread requestWriterThread = new ClientRequestThread(requestWriter, jobType);
-            Thread responseReaderThread = new ClientResponseThread(responseReader);
-
-
-
-            requestWriterThread.start();
-            responseReaderThread.start();
-            responseReaderThread.join();
-            requestWriterThread.join();
-
+//            requestWriterThread.start();
+//            responseReaderThread.start();
+//            responseReaderThread.join();
+//            requestWriterThread.join();
 
 //            String masterResponse = (ClientResponseThread) responseReaderThread.getResponse();
-
-
-
-
-
-
 
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
-
 
 }
