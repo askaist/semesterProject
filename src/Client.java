a71bd08bee4ff371f13b718a7127c888aba0baed
AriBinyamin <aribinyamin@gmail.com>
2023-05-13 21:50:50 -0400

    I connected the Server and Client together. Multiple clients will be able to connect because we have a ServerThreads class that will ensure that the server can be connected to multiple clients Slaves class has been created so that we can create to slaves that are optimized for different jobs. User/ client can input job type. Input validation included in the client class

--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,17 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Client {
 
     public static void main(String[] args) {
 
-        args = new String[]{"127.0.0.1", "30121"};
+        args = new String[] { "127.0.0.1", "30121" };
 
         if (args.length != 2) {
             System.err.println("Usage: java EchoClient <host name> <port number>");
@@ -17,24 +21,24 @@ public class Client {
         String hostName = args[0];
         int portNumber = Integer.parseInt(args[1]);
 
-        String jobType;
+        AtomicReference<String> jobType = new AtomicReference<>();
 
         try (Socket clientSocket = new Socket(hostName, portNumber);
              PrintWriter requestWriter = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader responseReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-             Scanner scanner = new Scanner(System.in))
-        {
+             BufferedReader responseReader = new BufferedReader(
+                     new InputStreamReader(clientSocket.getInputStream()));
+             Scanner scanner = new Scanner(System.in)) {
             System.out.println("Connection established");
 
-            //Get input from client
-            System.out.println("Enter a job value: A or B ");
-            jobType = scanner.nextLine();
-            // sends message to server
-            requestWriter.println(jobType);
+            while (true) {
+                System.out.println("Enter a job value: A or B ");
+                jobType.set(scanner.nextLine());
 
-            if (!Objects.equals(jobType, "A") && !Objects.equals(jobType, "B")) {
-                System.err.println("Invalid job type submitted");
-                System.exit(1);
+                if (Objects.equals(jobType.get(), "A") || Objects.equals(jobType.get(), "B")) {
+                    break;
+                } else {
+                    System.err.println("Invalid job type submitted. Please try again.");
+                }
             }
 
             Thread clientRequestThread = new Thread(() -> {
@@ -42,7 +46,6 @@ public class Client {
                 System.out.println("Job sent to server");
             });
             clientRequestThread.start();
-
 
             Thread clientResponseThread = new Thread(() -> {
                 try {
--- src/threads/SlaveThread.java
+++ src/threads/SlaveThread.java
@@ -7,64 +7,64 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class SlaveThread extends Thread {
-    private Socket slave;
-    String jobType;
-    boolean jobCompleted = false;
-    int jobID;
+                private Socket slave;
+                String jobType;
+                boolean jobCompleted = false;
+                int jobID;
 
 
-    public SlaveThread(Socket slave) {
-        this.slave = slave;
-    }
-
-    public void setJobID(int jobID) {
-        this.jobID = jobID;
-    }
-
-    public boolean getJobCompleted() {
-        return jobCompleted;
-    }
-
-    public void setJobType(String jobType) {
-        this.jobType = jobType;
-    }
-
-    @Override
-    public void run() {
-        try (
-                PrintWriter out = new PrintWriter(slave.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(slave.getInputStream()))
-        ) {
-            System.out.println("Slave thread started");
-
-
-            while (true) {
-                while (jobType == null) {
-                    Thread.sleep(200);
+                public SlaveThread(Socket slave) {
+                    this.slave = slave;
                 }
 
-                // Send jobType to slave
-                out.println(jobType);
-                out.println(jobID);
-
-
-                // Read slave requests and send responses
-                System.out.println("wait for response from slave");
-                String inputLine;
-                inputLine = in.readLine();
-
-                if (inputLine.equals("job completed")) {
-                    System.out.println("slave completed job of id " + jobID);
-                    synchronized (this) {
-                        jobCompleted = true;
-                    }
-                    Thread.sleep(200);
-                    jobCompleted = false;
-                    jobType = null;
+                public void setJobID(int jobID) {
+                    this.jobID = jobID;
                 }
 
+                public boolean getJobCompleted() {
+                    return jobCompleted;
+                }
 
-            }
+                public void setJobType(String jobType) {
+                    this.jobType = jobType;
+                }
+
+                @Override
+                public void run() {
+                    try (
+                            PrintWriter out = new PrintWriter(slave.getOutputStream(), true);
+                            BufferedReader in = new BufferedReader(new InputStreamReader(slave.getInputStream()))
+                    ) {
+                        System.out.println("Slave thread started");
+
+
+                        while (true) {
+                            while (jobType == null) {
+                                Thread.sleep(200);
+                            }
+
+                            // Send jobType to slave
+                            out.println(jobType);
+                            out.println(jobID);
+
+
+                            // Read slave requests and send responses
+                            System.out.println("wait for response from slave");
+                            String inputLine;
+                            inputLine = in.readLine();
+
+                            if (inputLine.equals("job completed")) {
+                                System.out.println("slave completed job of id " + jobID);
+                                synchronized (this) {
+                                    jobCompleted = true;
+                                }
+                                Thread.sleep(200);
+                                jobCompleted = false;
+                                jobType = null;
+                            }
+
+
+                        }
 
 
         } catch (IOException | InterruptedException e) {
